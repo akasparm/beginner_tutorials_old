fb8affd0ffabe3da0bf998bf23fdebe16f80dca8
Akashkumar Parmar <112916785+akasparm@users.noreply.github.com>
2023-11-03 12:26:36 -0400

    cpplint error resolution trial 1

--- cpp_pubsub/src/publisher_member_function.cpp
+++ cpp_pubsub/src/publisher_member_function.cpp
@@ -20,7 +20,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-using namespace std::chrono_literals;
+using std::chrono::operator""ms;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
  * member function as a callback from the timer. */
